eccece1a063c4b3537679e30e0bc4a45f8749f73
Cameron Cabe <bunneng@Camerons-MBP.lan>
2024-02-18 16:15:34 +0700

    hello world

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -13,7 +13,27 @@ class CategoryFactory extends Factory
     protected $model  = Category::class;
     public function definition()
     {
-        $categoryNames = ['Soft Drink', 'Energy Drink', 'Beer', 'Soda', 'Juice', 'Water', 'Tea', 'Coffee'];
+        $categoriesNames = array(
+            "Produce",
+            "Dairy",
+            "Meat",
+            "Bakery",
+            "Frozen",
+            "Canned",
+            "Grains",
+            "Snacks"
+        );
+
+        // No random category
+        // Product add new field ( bar code )
+        // Put payment method in order 
+        // Put order detail in order
+        // ENUM use UPPERCASE
+
+        // send customer with order detail when get all ( just name of customer )
+        // if tver lern: filter order by customer name, order date, order amount sort ASC DESC 
+
+        // $categoryNames = ['Beverages', 'Energy Drink', 'Beer', 'Soda', 'Juice', 'Water', 'Tea', 'Coffee'];
         return [
             'name' => $this->faker->randomElement($categoryNames),
             'description' => $this->faker->sentence,
